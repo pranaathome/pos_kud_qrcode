990e7268ad39538ac888e8059e47505479c82bcb
Ednoer <enurhadi24@gmail.com>
2018-09-22 10:22:45 +0700

    Model Category name description

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -12,4 +12,26 @@ class CategoryController extends Controller
         $categories = Category::orderBy('created_at', 'DESC')->paginate(10);
         return view('categories.index', compact('categories'));
     }
+
+    public function store(Request $request)
+    {
+
+        //form validation
+        $this->validate($request, [
+            'name' => 'required|string|max:50',
+            'description' => 'nullable|string'
+        ]);
+
+        try {
+            //firstOrCreate jika nama kategori sudah ada, maka record tersebut hanya diselect tidak di add
+            $categories = Category::firstOrCreate([
+                'name' => $request->name
+            ], [
+                'description' => $request->description
+            ]);
+            return redirect()->back()->with(['success' => 'Kategori: ' . $categories->name . ' Ditambahkan']);
+        } catch(\Exception $e) {
+            return redirect()->back()->with(['error' => $e->getMessage()]);
+        }
+    }
 }
