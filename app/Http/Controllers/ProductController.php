7cf06da1239e89db5b9b78f0379d48b82e8997a4
Ednoer <enurhadi24@gmail.com>
2018-09-23 03:13:16 +0700

    update product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -106,5 +106,49 @@ class ProductController extends Controller
         $categories = Category::orderBy('name', 'ASC')->get();
         return view('products.edit', compact('product', 'categories'));
     }
+
+    public function update(Request $request, $id)
+    {
+        //validasi
+        $this->validate($request, [
+            'code' => 'required|string|max:10|exists:products,code',
+            'name' => 'required|string|max:100',
+            'description' => 'nullable|string|max:100',
+            'stock' => 'required|integer',
+            'price' => 'required|integer',
+            'category_id' => 'required|exists:categories,id',
+            'photo' => 'nullable|image|mimes:jpg,png,jpeg'
+        ]);
+
+        try {
+            //query select berdasarkan id
+            $product = Product::findOrFail($id);
+            $photo = $product->photo;
     
+     
+            //cek jika ada file yang dikirim dari form
+            if ($request->hasFile('photo')) {
+                //cek, jika photo tidak kosong maka file yang ada di folder uploads/product akan dihapus
+                !empty($photo) ? File::delete(public_path('uploads/product/' . $photo)):null;
+                //uploading file dengan menggunakan method saveFile() yg telah dibuat sebelumnya
+                $photo = $this->saveFile($request->name, $request->file('photo'));
+            }
+
+            //perbaharui data di database
+            $product->update([
+                'name' => $request->name,
+                'description' => $request->description,
+                'stock' => $request->stock,
+                'price' => $request->price,
+                'category_id' => $request->category_id,
+                'photo' => $photo
+            ]);
+
+            return redirect(route('produk.index'))
+                ->with(['success' => '<strong>' . $product->name . '</strong> Diperbaharui']);
+            } catch (\Exception $e) {
+            return redirect()->back()
+                ->with(['error' => $e->getMessage()]);
+        }
+    }
 }
