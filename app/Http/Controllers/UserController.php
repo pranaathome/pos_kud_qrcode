5eeb420a273673825abb48ebab8128b9adc2e063
Ednoer <enurhadi24@gmail.com>
2018-09-24 20:13:00 +0700

    management user user controller, user/index

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('created_at', 'DESC')->paginate(10);
+        return view('users.index', compact('users'));
+    }
+    
+    public function create()
+    {
+        $role = Role::orderBy('name', 'ASC')->get();
+        return view('users.create', compact('role'));
+    }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:100',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+            'role' => 'required|string|exists:roles,name'
+        ]);
+
+        $user = User::firstOrCreate([
+            'email' => $request->email
+        ], [
+            'name' => $request->name,
+            'password' => bcrypt($request->password),
+            'status' => true
+        ]);
+
+        $user->assignRole($request->role);
+        return redirect(route('users.index'))->with(['success' => 'User: <strong>' . $user->name . '</strong> Ditambahkan']);
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:100',
+            'email' => 'required|email|exists:users,email',
+            'password' => 'nullable|min:6',
+        ]);
+
+        $user = User::findOrFail($id);
+        $password = !empty($request->password) ? bcrypt($request->password):$user->password;
+        $user->update([
+            'name' => $request->name,
+            'password' => $password
+        ]);
+        return redirect(route('users.index'))->with(['success' => 'User: <strong>' . $user->name . '</strong> Diperbaharui']);
+    }
+    
+    public function destroy($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+        return redirect()->back()->with(['success' => 'User: <strong>' . $user->name . '</strong> Dihapus']);
+    }
+
+}
