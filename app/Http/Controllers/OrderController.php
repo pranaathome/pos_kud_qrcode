7c92bc6dc52081347e78b57c92a1142089133704
Ednoer <enurhadi24@gmail.com>
2018-10-03 21:15:22 +0700

    vue gagal, ordercontroller vue

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -19,5 +19,61 @@ class OrderController extends Controller
         return response()->json($products, 200);
     }
     
+    public function addToCart(Request $request)
+    {
+        //validasi data yang diterima
+        //dari ajax request addToCart mengirimkan product_id dan qty
+        $this->validate($request, [
+            'product_id' => 'required|exists:products,id',
+            'qty' => 'required|integer'
+        ]);
+    
+     
+        //mengambil data product berdasarkan id
+        $product = Product::findOrFail($request->product_id);
+        //mengambil cookie cart dengan $request->cookie('cart')
+        $getCart = json_decode($request->cookie('cart'), true);
+
+        //jika datanya ada
+        if ($getCart) {
+            //jika key nya exists berdasarkan product_id
+            if (array_key_exists($request->product_id, $getCart)) {
+                //jumlahkan qty barangnya
+                $getCart[$request->product_id]['qty'] += $request->qty;
+                //dikirim kembali untuk disimpan ke cookie
+                return response()->json($getCart, 200)
+                    ->cookie('cart', json_encode($getCart), 120);
+            } 
+        }
+    
+     
+        //jika cart kosong, maka tambahkan cart baru
+        $getCart[$request->product_id] = [
+            'code' => $product->code,
+            'name' => $product->name,
+            'price' => $product->price,
+            'qty' => $request->qty
+        ];
+        //kirim responsenya kemudian simpan ke cookie
+        return response()->json($getCart, 200)
+            ->cookie('cart', json_encode($getCart), 120);
+    }
+
+    public function getCart()
+    {
+        //mengambil cart dari cookie
+        $cart = json_decode(request()->cookie('cart'), true);
+        //mengirimkan kembali dalam bentuk json untuk ditampilkan dengan vuejs
+        return response()->json($cart, 200);
+    }
+    
+    public function removeCart($id)
+    {
+        $cart = json_decode(request()->cookie('cart'), true);
+        //menghapus cart berdasarkan product_id
+        unset($cart[$id]);
+        //cart diperbaharui
+        return response()->json($cart, 200)->cookie('cart', json_encode($cart), 120);
+    }
 
 }
