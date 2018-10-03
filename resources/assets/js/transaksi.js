88543b2fe381305298b49ee8afd9768bfe88a6ca
Ednoer <enurhadi24@gmail.com>
2018-10-03 20:57:52 +0700

    vue belum bisa, tpi lanjut cart

--- resources/assets/js/transaksi.js
+++ resources/assets/js/transaksi.js
@@ -1,19 +1,33 @@
 import Vue from 'vue'
 import axios from 'axios'
 ​
+//import sweetalert library
+import VueSweetalert2 from 'vue-sweetalert2';
+​
 Vue.filter('currency', function (money) {
     return accounting.formatMoney(money, "Rp ", 2, ".", ",")
 })
+​
+//use sweetalert
+Vue.use(VueSweetalert2);
+​
 new Vue({
     el: '#dw',
     data: {
         product: {
             id: '',
-            qty: '',
             price: '',
             name: '',
             photo: ''
-        }
+        },
+        //menambahkan cart
+        cart: {
+            product_id: '',
+            qty: 1
+        },
+        //untuk menampung list cart
+        shoppingCart: [],
+        submitCart: false
     },
     watch: {
         //apabila nilai dari product > id berubah maka
@@ -34,6 +48,9 @@ new Vue({
             //akan disimpan di dalam var product > id
             this.product.id = $('#product_id').val();
         });
+        
+        //memanggil method getCart() untuk me-load cookie cart
+        this.getCart()
     },
     methods: {
         getProduct() {
@@ -43,6 +60,81 @@ new Vue({
             .then((response) => {
                 //assign data yang diterima dari server ke var product
                 this.product = response.data
+            })
+        },
+        
+        //method untuk menambahkan product yang dipilih ke dalam cart
+        addToCart() {
+            this.submitCart = true;
+            
+            //send data ke server
+            axios.post('/api/cart', this.cart)
+            .then((response) => {
+                setTimeout(() => {
+                    //apabila berhasil, data disimpan ke dalam var shoppingCart
+                    this.shoppingCart = response.data
+                    
+                    //mengosongkan var
+                    this.cart.product_id = ''
+                    this.cart.qty = 1
+                    this.product = {
+                        id: '',
+                        price: '',
+                        name: '',
+                        photo: ''
+                    }
+                    $('#product_id').val('')
+                    this.submitCart = false
+                }, 2000)
+            })
+            .catch((error) => {
+​
+            })
+        },
+        
+        //mengambil list cart yang telah disimpan
+        getCart() {
+            //fetch data ke server
+            axios.get('/api/cart')
+            .then((response) => {
+                //data yang diterima disimpan ke dalam var shoppingCart
+                this.shoppingCart = response.data
+            })
+        },
+        
+        //menghapus cart
+        removeCart(id) {
+            //menampilkan konfirmasi dengan sweetalert
+            this.$swal({
+                title: 'Kamu Yakin?',
+                text: 'Kamu Tidak Dapat Mengembalikan Tindakan Ini!',
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Iya, Lanjutkan!',
+                cancelButtonText: 'Tidak, Batalkan!',
+                showCloseButton: true,
+                showLoaderOnConfirm: true,
+                preConfirm: () => {
+                    return new Promise((resolve) => {
+                        setTimeout(() => {
+                            resolve()
+                        }, 2000)
+                    })
+                },
+                allowOutsideClick: () => !this.$swal.isLoading()
+            }).then ((result) => {
+                //apabila disetujui
+                if (result.value) {
+                    //kirim data ke server
+                    axios.delete(`/api/cart/${id}`)
+                    .then ((response) => {
+                        //load cart yang baru
+                        this.getCart();
+                    })
+                    .catch ((error) => {
+                        console.log(error);
+                    })
+                }
             })
         }
     }
--- resources/views/orders/add.blade.php
+++ resources/views/orders/add.blade.php
@@ -5,7 +5,7 @@
 @endsection
 ​
 @section('css')
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />  <!-- select box bisa melakukan filtering -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css" rel="stylesheet" />
 @endsection
 ​
 @section('content')
@@ -37,24 +37,35 @@
 ​
                             <div class="row">
                                 <div class="col-md-4">
-                                    <div class="form-group">
-                                        <label for="">Produk</label>
-                                        <select name="product_id" id="product_id" class="form-control" required width="100%">
-                                            <option value="">Pilih</option>
-                                            @foreach ($products as $product)
-                                            <option value="{{ $product->id }}">{{ $product->code }} - {{ $product->name }}</option>
-                                            @endforeach
-                                        </select>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="">Qty</label>
-                                        <input type="number" name="qty" id="qty" value="1" min="1" class="form-control">
-                                    </div>
-                                    <div class="form-group">
-                                        <button class="btn btn-primary btn-sm">
-                                            <i class="fa fa-shopping-cart"></i> Ke Keranjang
-                                        </button>
-                                    </div>
+                                    
+                                    <!-- SUBMIT DIJALANKAN KETIKA TOMBOL DITEKAN -->
+                                    <form action="#" @submit.prevent="addToCart" method="post">
+                                        <div class="form-group">
+                                            <label for="">Produk</label>
+                                            <select name="product_id" id="product_id"
+                                                v-model="cart.product_id"
+                                                class="form-control" required width="100%">
+                                                <option value="">Pilih</option>
+                                                @foreach ($products as $product)
+                                                <option value="{{ $product->id }}">{{ $product->code }} - {{ $product->name }}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="">Qty</label>
+                                            <input type="number" name="qty"
+                                                v-model="cart.qty" 
+                                                id="qty" value="1" 
+                                                min="1" class="form-control">
+                                        </div>
+                                        <div class="form-group">
+                                            <button class="btn btn-primary btn-sm"
+                                                :disabled="submitCart"
+                                                >
+                                                <i class="fa fa-shopping-cart"></i> @{{ submitCart ? 'Loading...':'Ke Keranjang' }}
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
                                 
                                 <!-- MENAMPILKAN DETAIL PRODUCT -->
@@ -91,6 +102,50 @@
                             </div>
                             @slot('footer')
 ​
+                            @endslot
+                        @endcard
+                    </div>
+                    
+                    <!-- MENAMPILKAN LIST PRODUCT YANG ADA DI KERANJANG -->
+                    <div class="col-md-4">
+                        @card
+                            @slot('title')
+                            Keranjang
+                            @endslot
+​
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Produk</th>
+                                        <th>Harga</th>
+                                        <th>Qty</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <!-- MENGGUNAKAN LOOPING VUEJS -->
+                                    <tr v-for="(row, index) in shoppingCart">
+                                        <td>@{{ row.name }} (@{{ row.code }})</td>
+                                        <td>@{{ row.price | currency }}</td>
+                                        <td>@{{ row.qty }}</td>
+                                        <td>
+                                            <!-- EVENT ONCLICK UNTUK MENGHAPUS CART -->
+                                            <button 
+                                                @click.prevent="removeCart(index)"    
+                                                class="btn btn-danger btn-sm">
+                                                <i class="fa fa-trash"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            @slot('footer')
+                            <div class="card-footer text-muted">
+                                <a href="{{ route('order.checkout') }}" 
+                                    class="btn btn-info btn-sm float-right">
+                                    Checkout
+                                </a>
+                            </div>
                             @endslot
                         @endcard
                     </div>
